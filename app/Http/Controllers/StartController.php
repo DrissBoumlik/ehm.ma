1df3cc8152e25cf84ccb7843e83484374ac28638
Driss Boumlik <boumlik.t@gmail.com>
2019-07-15 13:17:15 +0100

    Add Controllers Events / Products

---
+++ app/Http/Controllers/StartController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class StartController extends Controller
+{
+    public function getCities()
+    {
+        $regions_json_file = file_get_contents(storage_path('app\public') . '\json\region.json', true);
+        $cities_json_file = file_get_contents(storage_path('app\public') . '\json\ville.json', true);
+        $regions = json_decode($regions_json_file)->region;
+        $cities = json_decode($cities_json_file)->ville;
+        $cities = collect($cities)->groupBy('region')->sortKeys();
+        $regions = collect($regions)->groupBy('id')->sortKeys();
+        // dd($cities['6']);
+        // dd($cities['6'][0]);
+        // dd($regions['1'][0]->region);
+
+        return view('cities/index', ['cities' => $cities, 'regions' => $regions]);
+
+    }
+}
