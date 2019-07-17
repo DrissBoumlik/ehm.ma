82acd62ffc597ca27243ae66d0198dad16b4a641
Driss Boumlik <boumlik.t@gmail.com>
2019-07-17 17:11:45 +0100

    Add protected keyword to fillable field

--- app/Event.php
+++ app/Event.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model
 {
-    $fillable = ['title', 'description', 'images', 'start_date', 'end_date'];
+    protected $fillable = ['title', 'description', 'images', 'start_date', 'end_date'];
 }
