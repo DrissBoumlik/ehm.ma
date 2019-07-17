ad2973dfa98f2a021405926c7fdf55f6d52e0ab4
Driss Boumlik <boumlik.t@gmail.com>
2019-07-17 17:09:01 +0100

    Add headline view for internal pages

---
+++ resources/views/layouts/headline.blade.php
@@ -0,0 +1,18 @@
+<!-- Banner Section Start -->
+<div id="banner">
+    <div class="container">
+        <div class="row padding-100">
+            <div class="col-md-6 col-sm-6">
+                <h2 class="page-title">Events</h2>
+            </div>
+            <div class="col-md-6 col-sm-6">
+                <ul class="page-location">
+                    <li><a href="/">Accueil</a></li>
+                    <li><a href="#">.</a></li>
+                    <li><a class="active" href="#">Events</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Banner Section End -->
