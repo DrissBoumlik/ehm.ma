652e9549d33833f2ecb3a1c20a53b892f348d021
Driss Boumlik <boumlik.t@gmail.com>
2019-07-24 19:58:24 +0100

    Translate reste section of home page

--- resources/lang/en/file.php
+++ resources/lang/en/file.php
@@ -38,6 +38,78 @@ return [
         ],
         'products' => [
 
+        ],
+        'mission' => [
+            'header' => 'every where in morocco',
+            'text' => 'Elite Harvest Morocco aims to write a new green history on the lands of Morocco.
+                    We are one of the leading producers and distributors of fresh fruit daily in the region and
+                    our main goal is always quality and affordability. We are engaged in growing, sourcing,
+                    importing and marketing a wide range of high quality agricultural products through world class
+                    technology and efficient distribution chain with the use the most economical of our natural
+                    resources conserving water, energy and the earth.'
+        ],
+        'our-services' => [
+            'header' => 'our services',
+            'services' => [
+                'service-0' => [
+                    'title' => 'Management of agricultural projects',
+                    'description' => 'We undertake large scale agriculture and have the ability to provide expert advice and quality service ...'
+                ],
+                'service-1' => [
+                    'title' => 'Agricultural development',
+                    'description' => 'We use new technologies with a sharp focus on detailed farming practices aimed at maximizing the amount of production ...'
+                ],
+                'service-2' => [
+                    'title' => 'Farm Management',
+                    'description' => 'We offer productive and profitable agricultural management services using the latest technologies, combining local and international resources ...'
+                ],
+                'service-3' => [
+                    'title' => 'Irrigation consulting',
+                    'description' => 'Most agricultural enterprises, large or small, depend on irrigation to produce fresh fruits and vegetables of high quality. But there are several challenges to overcome...'
+                ],
+                'service-4' => [
+                    'title' => 'Laboratory Services',
+                    'description' => 'We want to contribute to the import of the latest agricultural technologies and know-how to the country and also encourage and support to adapt them to the conditions of Morocco ...'
+                ],
+            ]
+        ],
+        'our-values' => [
+            'header' => 'our values',
+            'values' => [
+                'value-0' => [
+                    'title' => 'Quality adherence',
+                    'icon' => 'flaticon-nature-3',
+                    'button' => 'read more'
+                ],
+                'value-1' => [
+                    'title' => 'Fostering agricultural growth through innovation',
+                    'icon' => 'flaticon-travel',
+                    'button' => 'read more'
+                ],
+                'value-2' => [
+                    'title' => 'Work with sustainability in mind',
+                    'icon' => 'flaticon-nature-1',
+                    'button' => 'read more'
+                ],
+                'value-3' => [
+                    'title' => 'Act with integrity',
+                    'icon' => 'flaticon-plant',
+                    'button' => 'read more'
+                ],
+                'value-4' => [
+                    'title' => 'Commitment to social responsibility',
+                    'icon' => 'flaticon-plant',
+                    'button' => 'read more'
+                ],
+
+            ]
+        ],
+        'events' => [
+            'header' => 'events',
+            'button' => 'see more'
         ]
-    ]
+    ],
+    'products-page' => [
+        'all-products' => 'all products'
+    ],
 ];
--- resources/lang/fr/file.php
+++ resources/lang/fr/file.php
@@ -35,6 +35,83 @@ return [
             'brand' => 'elite harvest',
             'text-1' => 'Elite Harvest Maroc (EHM) est l\'un des principaux producteurs et distributeurs de fruits frais de la région. Nos principaux objectifs sont la qualité et l’accessibilité économique. Nous cultivons, recherchons, importons et commercialisons une vaste gamme de produits agricoles de haute qualité grâce à une technologie de classe mondiale et à une chaîne de distribution efficace.',
             'text-2' => 'Nous accordons également la priorité à l\'utilisation économique de nos précieuses ressources naturelles - eau, énergie et terres.'
+        ],
+        'products' => [
+
+        ],
+        'mission' => [
+            'header' => 'partout au maroc',
+            'text' => 'Elite Harvest Maroc vise l’écriture d’une nouvelle histoire verte sur les terres
+                    du Maroc. On est l’un des principaux producteurs et distributeurs des fruits
+                    frais quotidiens dans la région et notre objectif principal est toujours la
+                    qualité et l’abordabilité. On s’engage dans la croissance , l’approvisionnement,
+                    l’importation et la commercialisation d’une vaste gamme de produits agricoles
+                    d’une haute qualité à travers la technologie de la classe mondiale et l’efficace
+                    chaine de distribution avec l’utilisation la plus économique de notre ressources
+                    naturelles conservant l’eau, l’énergie et la terre.'
+        ],
+        'our-services' => [
+            'header' => 'nos services',
+            'services' => [
+                'service-0' => [
+                    'title' => 'Gestion des projets agricoles',
+                    'description' => 'Nous entreprenons l’agriculture à grande échelle et possédons la capacité de fournir des conseils spécialisés et un service de qualité ...'
+                ],
+                'service-1' => [
+                    'title' => 'Développemene agricole',
+                    'description' => 'FamNous utilisons les nouvelles technologies avec un accent pointu sur les pratiques agricoles détaillées visant à maximiser la quantité de production...'
+                ],
+                'service-2' => [
+                    'title' => 'Gestion des fermes',
+                    'description' => 'Nous offrons des services de gestion agricole productifs et rentables en utilisant les dernières technologies, combinant les ressources locales et internationales…'
+                ],
+                'service-3' => [
+                    'title' => 'Conseil en irrigation',
+                    'description' => 'La plupart des entreprises agricoles, grandes ou petites, dépendent de l’irrigation pour produire des fruits et légumes frais de grande qualité. Mais il y a plusieurs défis à relever…'
+                ],
+                'service-4' => [
+                    'title' => 'Services de laboratoire',
+                    'description' => 'Nous voulons contribuer à l’importation des dernières technologies agricoles et du savoir-faire vers le pays et aussi encourager et soutenir pour les adapter aux conditions du Maroc…'
+                ],
+            ]
+        ],
+        'our-values' => [
+            'header' => 'nos valeurs',
+            'values' => [
+                'value-0' => [
+                    'title' => 'Adhésion à la qualité',
+                    'icon' => 'flaticon-nature-3',
+                    'button' => 'read more'
+                ],
+                'value-1' => [
+                    'title' => 'Favoriser la croissance agricole par l’innovation',
+                    'icon' => 'flaticon-travel',
+                    'button' => 'read more'
+                ],
+                'value-2' => [
+                    'title' => 'Travailler avec la durabilité à l’esprit',
+                    'icon' => 'flaticon-nature-1',
+                    'button' => 'read more'
+                ],
+                'value-3' => [
+                    'title' => 'Agir avec intégrité',
+                    'icon' => 'flaticon-plant',
+                    'button' => 'read more'
+                ],
+                'value-4' => [
+                    'title' => 'Commitment to social responsibility',
+                    'icon' => 'flaticon-plant',
+                    'button' => 'read more'
+                ],
+
+            ]
+        ],
+        'events' => [
+            'header' => 'événements',
+            'button' => 'voir plus'
         ]
+        ],
+    'products-page' => [
+        'all-products' => 'tous les produits'
     ]
 ];
