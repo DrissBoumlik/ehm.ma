d989e2948e5c9acac86d4fe3024a9d16c0002c23
Driss Boumlik <boumlik.t@gmail.com>
2019-08-01 10:10:08 +0100

    Add mail templates for contact / career

---
+++ resources/views/mail/application-mail.blade.php
@@ -0,0 +1,8 @@
+<p>{{ $first_name }}</p>
+<p>{{ $last_name }}</p>
+<p>{{ $email }}</p>
+<p>{{ $phone }}</p>
+<p>{{ $job }}</p>
+<p>{{ $diploma }}</p>
+<p>{{ $years_exp }}</p>
+<p>{{ $application_message }}</p>
---
+++ resources/views/mail/contact-mail.blade.php
@@ -0,0 +1,5 @@
+<p>{{ $name }}</p>
+<p>{{ $email }}</p>
+<p>{{ $subject }}</p>
+<p>{{ $contact_message }}</p>
+
