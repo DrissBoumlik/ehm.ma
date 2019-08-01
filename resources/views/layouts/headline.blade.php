250e1759d82661be5258e3f81cfef8121c382ef7
Driss Boumlik <boumlik.t@gmail.com>
2019-08-01 10:09:27 +0100

    Customize headline layout

--- resources/views/layouts/headline.blade.php
+++ resources/views/layouts/headline.blade.php
@@ -3,13 +3,13 @@
     <div class="container">
         <div class="row padding-100">
             <div class="col-md-6 col-sm-6">
-                <h2 class="page-title">Events</h2>
+                <h2 class="page-title capitalize">{{ $headline }}</h2>
             </div>
             <div class="col-md-6 col-sm-6">
                 <ul class="page-location">
-                    <li><a href="/">Accueil</a></li>
+                    <li><a class="capitalize" href="/">{{ trans('file.menu-top.home') }}</a></li>
                     <li><a href="#">.</a></li>
-                    <li><a class="active" href="#">Events</a></li>
+                    <li><a class="active capitalize" href="#">{{ $headline }}</a></li>
                 </ul>
             </div>
         </div>
