c3a9141ddf6ea43525a8d5e61491b664b0399b10
Driss <drissboumlik.dev@gmail.com>
2024-03-21 21:34:19 +0000

    Update

--- public/js/script.js
+++ public/js/script.js
@@ -115,8 +115,8 @@ $(function () {});
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! E:\C_Drive\Work\Job\CREAWAY\EHM\ehm-laravel\resources\js\script.js */"./resources/js/script.js");
-module.exports = __webpack_require__(/*! E:\C_Drive\Work\Job\CREAWAY\EHM\ehm-laravel\resources\sass\custom-style-bundle.sass */"./resources/sass/custom-style-bundle.sass");
+__webpack_require__(/*! D:\Code\Work\EHM\ehm-laravel\resources\js\script.js */"./resources/js/script.js");
+module.exports = __webpack_require__(/*! D:\Code\Work\EHM\ehm-laravel\resources\sass\custom-style-bundle.sass */"./resources/sass/custom-style-bundle.sass");
 
 
 /***/ })
